ee1a98df49c55b8247a2494378209aee68a32afa
Davide P <davidepassarini96@gmail.com>
2023-12-05 20:32:42 +0100

    added latest projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,267 +23,309 @@ import {
   jobit,
   tripguide,
   dashd,
+  restaurant,
+  gym,
   threejs,
-} from "../assets"
+} from '../assets';
 
 export const navLinks = [
   {
-    id: "about",
-    title: "About",
+    id: 'about',
+    title: 'About',
   },
   {
-    id: "work",
-    title: "Work",
+    id: 'work',
+    title: 'Work',
   },
   {
-    id: "contact",
-    title: "Contact",
+    id: 'contact',
+    title: 'Contact',
   },
-]
+];
 
 const services = [
   {
-    title: "Web Developer",
+    title: 'Web Developer',
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: 'React Native Developer',
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: 'Backend Developer',
     icon: backend,
   },
   {
-    title: "Angular Developer",
+    title: 'Angular Developer',
     icon: creator,
   },
-]
+];
 
 const technologies = [
   {
-    name: "HTML 5",
+    name: 'HTML 5',
     icon: html,
   },
   {
-    name: "CSS 3",
+    name: 'CSS 3',
     icon: css,
   },
   {
-    name: "JavaScript",
+    name: 'JavaScript',
     icon: javascript,
   },
   {
-    name: "TypeScript",
+    name: 'TypeScript',
     icon: typescript,
   },
   {
-    name: "React JS",
+    name: 'React JS',
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
+    name: 'Redux Toolkit',
     icon: redux,
   },
   {
-    name: "Tailwind CSS",
+    name: 'Tailwind CSS',
     icon: tailwind,
   },
   {
-    name: "Node JS",
+    name: 'Node JS',
     icon: nodejs,
   },
   {
-    name: "MongoDB",
+    name: 'MongoDB',
     icon: mongodb,
   },
   {
-    name: "Three JS",
+    name: 'Three JS',
     icon: threejs,
   },
   {
-    name: "git",
+    name: 'git',
     icon: git,
   },
   {
-    name: "figma",
+    name: 'figma',
     icon: figma,
   },
   {
-    name: "docker",
+    name: 'docker',
     icon: docker,
   },
-]
+];
 
 const experiences = [
   {
-    title: "Company Owner",
-    company_name: "My own Carpentry Shop",
+    title: 'Company Owner',
+    company_name: 'My own Carpentry Shop',
     icon: immagine,
-    iconBg: "#383E56",
-    date: "Oct 2015 - Jan 2022",
+    iconBg: '#383E56',
+    date: 'Oct 2015 - Jan 2022',
     points: [
-      "Carpentry experience honed my customer communication skills—understanding their needs, setting realistic expectations, and maintaining transparent dialogues.",
+      'Carpentry experience honed my customer communication skills—understanding their needs, setting realistic expectations, and maintaining transparent dialogues.',
       "I've learned the art of setting and managing expectations, avoiding overpromising, and ensuring delivery aligns with commitments, a crucial skill that applies in software development as well.",
-      "Regularly updating clients or team members on project progress enhances transparency and trust, a practice carried over from carpentry to my development work.",
-      "Obtaining milestone approvals from clients ensures project alignment and prevents misunderstandings—this practice is beneficial in both carpentry and development.",
+      'Regularly updating clients or team members on project progress enhances transparency and trust, a practice carried over from carpentry to my development work.',
+      'Obtaining milestone approvals from clients ensures project alignment and prevents misunderstandings—this practice is beneficial in both carpentry and development.',
     ],
   },
   {
-    title: "Front End Developer",
-    company_name: "Genesi Development",
+    title: 'Front End Developer',
+    company_name: 'Genesi Development',
     icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2022 -  Sep 2022",
+    iconBg: '#E6DEDD',
+    date: 'Jan 2022 -  Sep 2022',
     points: [
-      "Mastered responsive design for seamless user experiences on diverse devices.",
-      "Improved website layouts for better usability and visual appeal.",
-      "Contributed to faster-loading websites and enhanced user experiences through performance optimizations.",
-      "Worked effectively with design teams, refining skills in user-friendly interface development.",
+      'Mastered responsive design for seamless user experiences on diverse devices.',
+      'Improved website layouts for better usability and visual appeal.',
+      'Contributed to faster-loading websites and enhanced user experiences through performance optimizations.',
+      'Worked effectively with design teams, refining skills in user-friendly interface development.',
     ],
   },
   {
-    title: "Salesforce Dev 1 Course",
-    company_name: "Visioture",
+    title: 'Salesforce Dev 1 Course',
+    company_name: 'Visioture',
     icon: shopify,
-    iconBg: "#383E56",
-    date: "Jun 2022 - Sep 2023",
+    iconBg: '#383E56',
+    date: 'Jun 2022 - Sep 2023',
     points: [
-      "Acquired expertise in the Salesforce platform, including data modeling, automation with workflows, and Process Builder.",
-      "Developed dynamic user interfaces using Visualforce and Lightning components, enhancing user experience.",
-      "Hone Apex programming skills, empowering the ability to create custom solutions and extensions.",
-      "Gained insights into data security and compliance best practices, ensuring adherence to industry standards.",
+      'Acquired expertise in the Salesforce platform, including data modeling, automation with workflows, and Process Builder.',
+      'Developed dynamic user interfaces using Visualforce and Lightning components, enhancing user experience.',
+      'Hone Apex programming skills, empowering the ability to create custom solutions and extensions.',
+      'Gained insights into data security and compliance best practices, ensuring adherence to industry standards.',
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Kamy Kommunikasjon",
+    title: 'Web Developer',
+    company_name: 'Kamy Kommunikasjon',
     icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Dec 2023 - Mar 2023",
+    iconBg: '#E6DEDD',
+    date: 'Dec 2023 - Mar 2023',
     points: [
-      "Strengthened collaboration skills through teamwork on international projects, fostering a global perspective.",
-      "Achieved proficiency as a developer by adhering to high coding standards and effectively resolving critical issues to improve user experiences.",
-      "Contributed to enhancing website user interfaces and structures, becoming a more well-rounded developer.",
-      "These experiences significantly elevated my skills, equipping me to excel in the tech industry.",
+      'Strengthened collaboration skills through teamwork on international projects, fostering a global perspective.',
+      'Achieved proficiency as a developer by adhering to high coding standards and effectively resolving critical issues to improve user experiences.',
+      'Contributed to enhancing website user interfaces and structures, becoming a more well-rounded developer.',
+      'These experiences significantly elevated my skills, equipping me to excel in the tech industry.',
     ],
   },
-]
+];
 
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Davide proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      'I thought it was impossible to make a website as beautiful as our product, but Davide proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Davide does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
   },
   {
     testimonial:
       "After Davide optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
   },
-]
+];
 
 const projects = [
   {
-    name: "Coffee App",
+    name: 'Coffee App',
     description:
-      "Angular web application that allows users to search and filter a list of coffee products. The app provides a user-friendly interface for searching coffees based on their names.",
+      'Angular web application that allows users to search and filter a list of coffee products. The app provides a user-friendly interface for searching coffees based on their names.',
     tags: [
       {
-        name: "angular",
-        color: "blue-text-gradient",
+        name: 'angular',
+        color: 'blue-text-gradient',
       },
       {
-        name: "components",
-        color: "green-text-gradient",
+        name: 'components',
+        color: 'green-text-gradient',
       },
       {
-        name: "api",
-        color: "pink-text-gradient",
+        name: 'api',
+        color: 'pink-text-gradient',
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/Zast96/CoffeeApp",
+    source_code_link: 'https://github.com/Zast96/CoffeeApp',
   },
   {
-    name: "Gallery Cards",
+    name: 'XPLR Site',
     description:
-      "Web application that enables users to scroll between a small gallery of images of Norwegian landscapes, with a smooth and material design thanks to Bootstrap.",
+      'A warm welcome to XPLR, a traveling website concept. Check out the most amazing places on earth and be astonished by the HD video of the Lofoten Islands (Thanks Git LFS)',
     tags: [
       {
-        name: "bootstrap",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "gallery",
-        color: "green-text-gradient",
+        name: 'gallery',
+        color: 'green-text-gradient',
       },
       {
-        name: "html",
-        color: "pink-text-gradient",
+        name: 'Git',
+        color: 'pink-text-gradient',
       },
     ],
     image: jobit,
-    source_code_link:
-      "https://github.com/Zast96/Bootstrap-Projects/tree/main/Cards",
+    source_code_link: 'https://github.com/Zast96/traveling_site',
   },
   {
-    name: "Location Weather",
+    name: 'BlockBuster Site',
     description:
-      "A small but effective app to check the current weather based on the location that you search. Also provides a minimal image with icons and temperature.",
+      'A small concept for a movie reviews website. The perfect example of semplicity and clearliness. This layout would have been perfect if BlockBuster was still alive.',
     tags: [
       {
-        name: "bootstrap",
-        color: "blue-text-gradient",
+        name: 'html',
+        color: 'blue-text-gradient',
       },
       {
-        name: "location",
-        color: "green-text-gradient",
+        name: 'list',
+        color: 'green-text-gradient',
       },
       {
-        name: "ui",
-        color: "pink-text-gradient",
+        name: 'ui',
+        color: 'pink-text-gradient',
       },
     ],
     image: tripguide,
-    source_code_link:
-      "https://github.com/Zast96/Bootstrap-Projects/tree/main/Weather%20Project",
+    source_code_link: 'https://github.com/Zast96/movies_site',
   },
   {
-    name: "Syncfusion Dashboard",
+    name: 'Syncfusion Dashboard',
     description:
-      "A cool dashboard that will cover all your needs if you manage an e-shop. Amazing charts are included, as well as other cool utilities.",
+      'A cool dashboard that will cover all your needs if you manage an e-shop. Amazing charts are included, as well as other cool utilities.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'clean',
+        color: 'blue-text-gradient',
       },
       {
-        name: "dashboard",
-        color: "green-text-gradient",
+        name: 'dashboard',
+        color: 'green-text-gradient',
       },
       {
-        name: "utilities",
-        color: "pink-text-gradient",
+        name: 'utilities',
+        color: 'pink-text-gradient',
       },
     ],
     image: dashd,
-    source_code_link: "https://github.com/Zast96/syncfusion_dashboard",
+    source_code_link: 'https://github.com/Zast96/syncfusion_dashboard',
   },
-]
+  {
+    name: 'Restaurant Site',
+    description:
+      'Welcome to my restaurant! I love this design, rich but simple. We can still do a lot with our dear old Html, Css, Js team(a pinch of Bootstrap may be present, be careful!).',
+    tags: [
+      {
+        name: 'modern',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'order',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'bootstrap',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: restaurant,
+    source_code_link: 'https://github.com/Zast96/restaurant_site',
+  },
+  {
+    name: 'Gym Site',
+    description:
+      'Usually developers are not gym types but after seeing this layout perhaps you will have more will to go to the gym. Or to stretch at home, at least.',
+    tags: [
+      {
+        name: 'fitness',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'red',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'fluid',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: gym,
+    source_code_link: 'https://github.com/Zast96/gym_site',
+  },
+];
 
-export { services, technologies, experiences, testimonials, projects }
+export {services, technologies, experiences, testimonials, projects};
