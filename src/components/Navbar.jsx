52850dec21f9d189e28a3d6c7a0ae480d9bcc0b7
Davide P <davidepassarini96@gmail.com>
2024-01-23 13:49:18 +0100

    Added Linked profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,36 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 
-import { styles } from '../styles'
-import { navLinks } from '../constants'
-import { logo, menu, close } from '../assets'
+import {styles} from '../styles';
+import {navLinks} from '../constants';
+import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('`')
-  const [toggle, setToggle] = useState(false)
-
+  const [active, setActive] = useState('`');
+  const [toggle, setToggle] = useState(false);
+  const handleJavascriptMasteryClick = () => {
+    // Redirect to the LinkedIn profile
+    window.location.href =
+      'https://www.linkedin.com/in/davide-passarini-374231241/';
+  };
   return (
-    <nav className="w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+    <nav className='w-full flex items-center py-5 fixed top-0 z-20 bg-primary'>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
-          to="/"
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive('')
-            window.scrollTo(0, 0)
-          }}
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => handleJavascriptMasteryClick()}
         >
-          <img src={logo} alt="logo" className="w-28 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+          <img src={logo} alt='logo' className='w-28 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
             Davide &nbsp;{' '}
-            <span className="sm:block hidden">| Javascript Mastery</span>
+            <span className='sm:block hidden'>| Linkedin Profile</span>
           </p>
         </Link>
 
-        <ul className="list-none hidden sm:flex flex-row gap-10 ">
+        <ul className='list-none hidden sm:flex flex-row gap-10 '>
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -43,11 +44,11 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
             src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
           <div
@@ -55,7 +56,7 @@ const Navbar = () => {
               !toggle ? 'hidden' : 'flex'
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className="list-none flex justify-end items-start flex-col gap-4 ">
+            <ul className='list-none flex justify-end items-start flex-col gap-4 '>
               {navLinks.map((link) => (
                 <li
                   key={link.id}
@@ -63,8 +64,8 @@ const Navbar = () => {
                     active === link.title ? 'text-white' : 'text-secondary'
                   } font-poppins font-medium cursor-pointer text-[16px]`}
                   onClick={() => {
-                    setToggle(!toggle)
-                    setActive(link.title)
+                    setToggle(!toggle);
+                    setActive(link.title);
                   }}
                 >
                   <a href={`#${link.id}`}>{link.title}</a>
@@ -75,7 +76,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
